347e83aa610e86177f2c9446acc82d9c61c6f21f
Sandeep Ramgolam <sandeep@ramgolam.com>
2021-11-08 10:05:22 +0400

    added vue-query usage

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import { useQueryProvider } from 'vue-query'
 
+useQueryProvider()
 </script>
 
 <template>
--- src/services/covid.api.ts
+++ src/services/covid.api.ts
@@ -1,6 +1,5 @@
-export const getCovidData = (countryName: string) => {
+export const getCovidData = () => {
   // eslint-disable-next-line no-console
-  console.log(countryName)
   return fetch('https://pomber.github.io/covid19/timeseries.json')
     .then(response => response.json())
 }
